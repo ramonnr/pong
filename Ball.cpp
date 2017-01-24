2cdb1003c579f1b62fd37a87d36f8b1a2b2d39ef
Ramón N. Rodriguez <ramonnr@kth.se>
2017-01-24 11:18:13 +0100

    fixed paddle-ball collision, made ball boring

--- Ball.cpp
+++ Ball.cpp
@@ -1,8 +1,8 @@
 #include "Ball.h"
 #include "Window.h"
 
-Ball::Ball(float paddleW, float paddleH, float paddleX) 
-	:mPaddleWidth(paddleW), mPaddleHeight(paddleH), mPaddleX(paddleX){
+Ball::Ball(float paddleW, float paddleH, float paddleX)
+	:mPaddleWidth(paddleW), mPaddleHeight(paddleH), mPaddleX(paddleX) {
 	srand(time(NULL));
 
 	mXMax = Window::getWindow().getSize().x;
@@ -22,28 +22,39 @@ void Ball::update(float paddleY) {
 }
 
 void Ball::draw(void) {
-	
+
 	move();
 	Window::getWindow().draw(mBall);
-	
+
 }
 
 void Ball::move(void) {
 	float xCurr = mBall.getPosition().x;
 	float yCurr = mBall.getPosition().y;
-	
+
 	bool collision = false;
-	if (xCurr == mPaddleX) {
-		if (yCurr > mPaddleY && yCurr < mPaddleY + mPaddleHeight)
+	if (xCurr < mPaddleX * 1.05 && xCurr > mPaddleX * 0.95) {
+		if (yCurr > mPaddleY - 1 && yCurr < (mPaddleY + mPaddleHeight + 1))
 			collision = true;
 	}
-	
 
-	if(xCurr +mBallR*2 >= mXMax -1 || xCurr == 0 || collision)
-		mHeadingX = rand() % 2 * mXMax;
-	if(yCurr + mBallR*2 >= mYMax -1 || yCurr == 0 || collision)
-		mHeadingY = rand() % 2 * mYMax;
+	if (xCurr == 0 || collision)
+		mHeadingX = mXMax;
+	else if (xCurr + mBallR * 2 >= mXMax - 1)
+		mHeadingX = 0;
 
+	if (yCurr == 0 || collision)
+		mHeadingY = mYMax;
+	else if (yCurr + mBallR * 2 >= mYMax - 1)
+		mHeadingY = 0;
+
+	/*
+	if( ((xCurr +mBallR*2) >= (mXMax -1)) || xCurr == 0 || collision)
+	mHeadingX = rand() % 2 * mXMax;
+	if( ((yCurr + mBallR*2) >= (mYMax -1)) || yCurr == 0 || collision)
+	mHeadingY = rand() % 2 * mYMax;
+
+	*/
 	if (mHeadingX < xCurr) xCurr = -mSpeed;
 	else xCurr = mSpeed;
 
@@ -51,6 +62,6 @@ void Ball::move(void) {
 	else yCurr = mSpeed;
 
 	mBall.move(xCurr, yCurr);
-	
+
 }
 
